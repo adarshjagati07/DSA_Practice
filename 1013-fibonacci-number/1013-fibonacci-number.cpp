3d3954bf4ce6b97c6bad6e990663909d37ae3b05
Adarsh Narayan Jagati <91245532+adarshjagati07@users.noreply.github.com>
2023-10-06 08:16:41 +0530

    Time: 0 ms (100%), Space: 6.3 MB (14.39%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,7 +1,15 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n<=1) return n;
-        return fib(n-1)+fib(n-2);
+        if(n==0||n==1){
+            return n;
+        }
+       int a=0,b=1,fibo=0;
+        for( int i=1;i<n;i++){
+           fibo=a+b;
+            a=b;
+            b=fibo;
+        }
+        return fibo;
     }
 };
