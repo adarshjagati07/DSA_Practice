371e5e3bf94368f3b43b9ffa2b027b3693cf00d2
Adarsh <itsadarsh0702@gmail.com>
2024-02-04 19:56:17 +0530

    check sorted array

---
+++ Check-sorted-array/code.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int isSorted(int n, vector<int> a) {
+    for(int i = 0; i < n-1; i++){
+        if(a[i]>a[i+1]){
+            return 0;
+        }
+    }
+    return 1;
+}
