75f0ad8804b74642997b483f173e362150325025
Adarsh Narayan Jagati <91245532+adarshjagati07@users.noreply.github.com>
2024-02-15 07:27:19 +0530

    Time: 0 ms (100.00%), Space: 14.5 MB (25.94%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,31 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(), nums.end());
+        int index = -1;
+        int numsLen = nums.size();
+        
+        //finding the breakpoint from last
+        for(int i = numsLen-2; i >= 0; i--){
+            if(nums[i] < nums[i+1]){
+                index = i;
+                break;
+            }
+        }
+        
+        //no breakpoint found
+        if(index == -1){
+            reverse(nums.begin(),nums.end());
+            return;
+        }
+        
+        //getting the smallest number greater than nums[index]
+        for(int i = numsLen-1; i > index; i--){
+            if(nums[i] > nums[index]){
+                swap(nums[i],nums[index]);
+                break;
+            }
+        }
+        //after that breakpoint
+        reverse(nums.begin() + index + 1, nums.end());
     }
 };
