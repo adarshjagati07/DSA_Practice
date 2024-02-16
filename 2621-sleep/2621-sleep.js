34aa2bad83836a76de3120e4c4cf86b9b597a831
Adarsh Narayan Jagati <91245532+adarshjagati07@users.noreply.github.com>
2024-02-16 06:42:16 +0530

    Time: 64 ms (12.94%), Space: 47.4 MB (44.67%) - LeetHub

---
+++ 2621-sleep/2621-sleep.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} millis
+ * @return {Promise}
+ */
+async function sleep(millis) {
+    
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            resolve(millis);
+        }, millis);
+    });
+    
+}
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
