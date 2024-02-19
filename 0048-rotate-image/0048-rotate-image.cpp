a290102da889b37266be962ff4fc2ee97c9a68df
Adarsh Narayan Jagati <91245532+adarshjagati07@users.noreply.github.com>
2024-02-19 07:24:23 +0530

    Time: 5 ms (6.39%), Space: 8.5 MB (47.98%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        int length = matrix.size();
-        vector<vector<int>> rotatedArr(length, vector<int>(length)); // Resize rotatedArr
+        //previous one was brute-force approach, now we'll see optimal solution
+        int length = matrix.size(); 
         
+        //firstly find the transpose: we'll get reversed matrix
         for(int i = 0; i < length; i++){
-            for(int j = 0; j < length; j++){
-                rotatedArr[j][(length-1) - i] = matrix[i][j];
+            for(int j = i+1; j < length; j++){
+                 int temp = matrix[i][j];
+                 matrix[i][j] = matrix[j][i];
+                 matrix[j][i] = temp;
             }
         }
-        matrix = rotatedArr;
+        //then reverse the matrix to get the answer
+        for (auto& row : matrix) {
+            reverse(row.begin(), row.end());
+        }
+        
+        
     }
 };
