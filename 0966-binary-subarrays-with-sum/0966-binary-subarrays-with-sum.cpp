ab54efc745c800ed658be68a95c3dc15d2f52efb
Adarsh Narayan Jagati <91245532+adarshjagati07@users.noreply.github.com>
2024-11-23 17:13:32 +0530

    Time: 15 ms (41.91%), Space: 38.5 MB (33.23%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
@@ -2,28 +2,18 @@ class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
         int numsLen = nums.size();
-        // our ans is going to f(nums,goal)-f(nums,goal-1);
-        int ans = countOfSubArrLessThanGoal(nums,goal,numsLen) - countOfSubArrLessThanGoal(nums,goal-1,numsLen);
-        return ans;
-    }
-
-    int countOfSubArrLessThanGoal(vector<int> &nums, int goal, int n){
-        int left = 0;
-        int right = 0;
-        int ans = 0;
         int sum = 0;
-
-        // this is the algo which will find us the subarrays for (sum <= goal);
-        while(right < n){
-            if(goal < 0) return 0;
-            sum += nums[right];
-
-            while(sum > goal){
-                sum -= nums[left];
-                left++;
+        int ans = 0;
+        unordered_map<int, int> prefixSumMapCount;
+        prefixSumMapCount[0] = 1;
+        int i = 0;
+        while(i < numsLen){
+            sum = sum + nums[i];
+            if(prefixSumMapCount.find(sum - goal) != prefixSumMapCount.end()){
+                ans += prefixSumMapCount[sum - goal];
             }
-            ans = ans + (right-left+1);
-            right++;
+            prefixSumMapCount[sum]++;
+            i++;
         }
         return ans;
     }
