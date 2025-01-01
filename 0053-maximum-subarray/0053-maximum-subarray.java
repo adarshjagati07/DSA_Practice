1d60a6759b882aec966ee39e2df56342ea37177b
Adarsh Narayan Jagati <91245532+adarshjagati07@users.noreply.github.com>
2025-01-01 09:21:15 +0530

    Time: 1 ms (99.39%), Space: 57.5 MB (11.71%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        //Kadane's algorithm
+        int maxSum = nums[0];
+        int sum = 0;
+
+        for(int i = 0; i < nums.length; i++){
+            sum+=nums[i];
+            maxSum = Math.max(sum, maxSum);
+            if(sum < 0){
+                sum = 0;
+            }
+        }
+        return maxSum;
+    }
+}
